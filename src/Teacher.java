5af6152916a2b1e21c1560bd2c90c23a1b8ba97d
kiko <kiko.binary@gmail.com>
2025-12-09 11:10:36 +0500

    Student java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,6 @@
 public class Teacher {
+    //	•	fullName (String) — ФИО
+    //	•	age (int) — возраст
+    //	•	grade (int) — класс (напр. 1–11)
+    //	•	gpa (double) — средний балл (0.0–5.0 или 0.0–4.0 — выберите одно)
 }
