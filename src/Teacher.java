cd74b7acd4626255895e0148d0beb0e0dab11e7b
kiko <kiko.binary@gmail.com>
2025-12-09 12:28:40 +0500

    teacher java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,5 @@
 public class Teacher {
-    //	•	fullName (String) — ФИО
-    //	•	age (int) — возраст
-    //	•	grade (int) — класс (напр. 1–11)
-    //	•	gpa (double) — средний балл (0.0–5.0 или 0.0–4.0 — выберите одно)
+
     private String fullname;
     private String subject;
     private int expirience;
@@ -26,6 +23,7 @@ public class Teacher {
     }
     public boolean hasMoreExperienceThan(Teacher other) {
         return this.expirience > other.expirience;
+
     }
 
 
